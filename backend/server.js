ee774dc74c905ab6f1bd952bbfde1ebd3fe9b02f
doraem-on <lr_be24@thapar.edu>
2025-12-28 11:23:55 +0530

    Fix meeting room logic and add comments

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,26 @@ import { connectDB } from './config/db.js';
 dotenv.config();
 const app = express();
 
-app.get('/products', (req, res) => {
- console.log(process.env.MONGO_URI);
+app.use(express.json());// Middleware to parse JSON bodies
+app.post('/api/products', async (req, res) =>{
+  const product = req.body;
+
+  if(!product.name || !product.price || !product.image) {
+    return res.status(400).json({ success: false, message: 'All fields are required' });
+  }
+
+  const newProduct = new Product(product);
+    try {
+      await newProduct.save();
+      res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+      console.error("Error in Create product:", error);
+      res.status(500).json({ success: false, message: 'Server Error' });
+    }
+  
 });
+
+
 app.listen(5000, () => {
   connectDB();
   console.log('Server is running on port 5000');
